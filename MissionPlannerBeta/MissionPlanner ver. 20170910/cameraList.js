e1c1c6d7fbfd05dd0da8546c692d08741e3ee163
oceam <guowei.net@hotmail.com>
2018-08-22 15:03:13 +0900

    added raw file

---
+++ MissionPlannerBeta/MissionPlanner ver. 20170910/cameraList.js
@@ -0,0 +1,65 @@
+var cameraList = [
+	{
+		name: 'ZENMUSE X5',
+		sensorX: 17.3,
+		sensorY: 13,
+		focusLength: 15,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'ZENMUSE X3',
+		sensorX: 6.17,
+		sensorY: 4.55,
+		focusLength: 2.88,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'Phantom3',
+		sensorX: 6.17,
+		sensorY: 4.55,
+		focusLength: 2.88,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'Phantom4',
+		sensorX: 6.17,
+		sensorY: 4.55,
+		focusLength: 2.88,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'Phantom4 Pro (3:2)',
+		sensorX: 13.2,
+		sensorY: 8.8,
+		focusLength: 8.6,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'RedEdge_5.5',
+		sensorX: 4.8,
+		sensorY: 3.6,
+		focusLength: 5.5,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'FLIR_19.0',
+		sensorX: 10.88,
+		sensorY: 8.704,
+		focusLength: 19,
+		fRatio: 'NaN'
+	},
+	{
+		name: 'EOS 5Ds R(EF 50mm F1.2)',
+		sensorX: 36,
+		sensorY: 24,
+		focusLength: 50,
+		fRatio: 1.2
+	},
+	{
+		name: 'EOS 5Ds R(EF 24mm F1.4)',
+		sensorX: 35,
+		sensorY: 24,
+		focusLength: 24,
+		fRatio: 1.4
+	}
+];
